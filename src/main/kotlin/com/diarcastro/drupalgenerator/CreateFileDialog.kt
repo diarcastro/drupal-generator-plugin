67223ede80d3b0e7377ea75c7600053d148c8f3b
Diego Castro <ing.diegocastro@gmail.com>
2025-02-15 22:30:08 -0500

    - Adding extra fields to UI

--- src/main/kotlin/com/diarcastro/drupalgenerator/CreateFileDialog.kt
+++ src/main/kotlin/com/diarcastro/drupalgenerator/CreateFileDialog.kt
@@ -1,7 +1,12 @@
 package com.diarcastro.drupalgenerator
 
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.ComboBox
+import java.awt.BorderLayout
+import java.awt.Dimension
 import java.awt.FlowLayout
+import java.awt.Font
+import javax.swing.BoxLayout
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JTextField
@@ -10,24 +15,61 @@ import javax.swing.SwingUtilities
 
 
 class CreateFileDialog : DialogWrapper(true) {
+    private var mainPanel: JPanel = JPanel()
     private var sdcName: JTextField = JTextField("my-sdc", 20)
     private var sdcLabel: JLabel = JLabel("SDC Name:")
-    private var contentPane: JPanel = JPanel(FlowLayout(FlowLayout.LEFT))
 
     init {
         init()
         title = "New Drupal SDC"
+    }
 
+    private fun createSection(title: String = "", vararg components: JComponent): JPanel {
+        val panel = JPanel(BorderLayout())
+        if (title.isNotEmpty()) {
+            val titleLabel = JLabel(title)
+            titleLabel.font = titleLabel.font.deriveFont(Font.BOLD, 14f)
+            panel.add(titleLabel, BorderLayout.NORTH)
+        }
+
+        val contentPanel = JPanel(FlowLayout(FlowLayout.LEFT, 15, 10))
+        components.forEach { contentPanel.add(it, BorderLayout.NORTH) }
+        panel.add(contentPanel, BorderLayout.CENTER)
+
+        return panel
     }
 
     override fun createCenterPanel(): JComponent {
-        contentPane.add(sdcLabel)
-        contentPane.add(sdcName)
+        mainPanel.layout = BoxLayout(mainPanel, BoxLayout.Y_AXIS)
+        mainPanel.minimumSize = Dimension(450, 300)
+
+        val panels = mutableListOf<JPanel>()
+
+        val statusLabel = JLabel("Status:")
+        val statusDropdown = ComboBox(arrayOf("experimental", "stable", "deprecated", "obsolete"))
+        statusDropdown.selectedItem = "stable"
+        val panelStatus = createSection(
+            "Component Details",
+            statusLabel,
+            statusDropdown,
+        )
+
+        panels.add(panelStatus)
+        val panelComponent = createSection(
+            "",
+            sdcLabel,
+            sdcName,
+        )
+        panels.add(panelComponent)
+
+
         SwingUtilities.invokeLater {
             sdcName.selectAll()
             sdcName.requestFocusInWindow()
         }
-        return contentPane
+        panels.forEach({ mainPanel.add(it, BorderLayout.NORTH) })
+
+        return mainPanel
     }
 
     fun getComponentData(): ComponentData {
