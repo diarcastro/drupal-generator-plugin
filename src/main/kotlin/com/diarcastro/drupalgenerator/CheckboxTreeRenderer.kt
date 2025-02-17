95ca52cb8ebfc9b2bb365429f800b6d4d44594e2
Diego Castro <ing.diegocastro@gmail.com>
2025-02-16 19:08:04 -0500

    - Adding icons to the tree

--- src/main/kotlin/com/diarcastro/drupalgenerator/CheckboxTreeRenderer.kt
+++ src/main/kotlin/com/diarcastro/drupalgenerator/CheckboxTreeRenderer.kt
@@ -1,8 +1,10 @@
 package com.diarcastro.drupalgenerator
 
+import com.intellij.openapi.util.IconLoader
 import com.intellij.ui.CheckboxTree
 import com.intellij.ui.CheckedTreeNode
 import com.intellij.ui.SimpleTextAttributes
+import javax.swing.Icon
 import javax.swing.JTree
 
 class CheckboxTreeRenderer : CheckboxTree.CheckboxTreeCellRenderer() {
@@ -17,8 +19,26 @@ class CheckboxTreeRenderer : CheckboxTree.CheckboxTreeCellRenderer() {
     ) {
         if (value is CheckedTreeNode) {
             val text = value.userObject.toString()
+            val icon: Icon;
+            icon = if (leaf) {
+                AppIcons.FILE_CODE
+            } else {
+                if (expanded) {
+                    AppIcons.FOLDER_OPEN
+                } else {
+                    AppIcons.FOLDER
+                }
+            }
+
+            textRenderer.icon = icon
             textRenderer.append(text, SimpleTextAttributes.REGULAR_ATTRIBUTES)
             textRenderer.append("  ")
         }
     }
+}
+
+object AppIcons {
+    val FOLDER: Icon = IconLoader.getIcon("icons/folder.png", AppIcons::class.java)
+    val FOLDER_OPEN: Icon = IconLoader.getIcon("icons/folder-open.png", AppIcons::class.java)
+    val FILE_CODE: Icon = IconLoader.getIcon("icons/file-code.png", AppIcons::class.java)
 }
